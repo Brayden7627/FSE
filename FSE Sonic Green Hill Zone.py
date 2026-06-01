642b2c8372a83a26542f0b907d8da247eb236f89
Brayden Wang <wbrayden2020@gmail.com>
2026-05-31 23:27:27 -0400

    commit

--- FSE Sonic Green Hill Zone.py
+++ FSE Sonic Green Hill Zone.py
@@ -20,7 +20,13 @@ white=(255,255,255)
 #Misc
 
 gravity=1
+X=0
+Y=1
+W=2
+H=3
 
+vel=[0,0,700]
+jumpPower=-25
 
 
 
@@ -28,14 +34,36 @@ gravity=1
 
 #Functions
 
-def drawScene(platforms):
+def drawScene(player,platforms):
+    draw.rect(screen,blue,player)
     for p in platforms:
         draw.rect(screen,green,p)
 
-def move():
+    
+
+def move(p):
     keys=key.get_pressed()
 
-    #if keys[K_SPACE] and vel[Y]
+    if keys[K_SPACE] and vel[Y]==0 and p[Y]+p[H]==vel[2]:
+        vel[Y]=jumpPower
+
+    vel[X]=0
+    if keys[K_LEFT]:
+        vel[X]=-5
+    if keys[K_RIGHT]:
+        vel[X]=5
+    p[X]+=vel[X]#horizontal movement
+
+    vel[Y]+=gravity#acceleration
+
+def check(p,plats):
+    'check if the players "lands" on one of the platforms'
+    for plat in plats:
+        if p[X]+p[W]>plat[X] and p[X]<plat[X]+plat[W] and p[Y]+p[H]<=plat[Y] and p[Y]+p[H]+vel[Y]>=plat[Y]:
+            vel[2]=plat[Y]
+            vel[Y]=0#stop moving
+            p[Y]=vel[2]-p[H]
+    p[Y]+=vel[Y]#vertical movement
 
 
 
@@ -44,26 +72,34 @@ def move():
 def instructions():
     draw.rect(screen,yellow,(0,0,width,height))
 
+
 def controls():
     draw.rect(screen,red,(0,0,width,height))
 
-def level1():
-    draw.rect(screen,white,(0,0,width,height))
 
+def level1():
     running=True
+    myClock=time.Clock()
+
     counter=0
 
     platforms=[Rect(100,700,1000,100)]
+    player=Rect(200,600,20,20)
 
     while running:
+
+        screen.fill(white)
+
         for evt in event.get():
             if evt.type==QUIT:
                 running=False
         
+        check(player,platforms)
+        move(player)
+        drawScene(player,platforms)
+        myClock.tick(60)
+
         counter+=1#frames
-        drawScene(platforms)
-
-
 
 
 
@@ -119,7 +155,7 @@ def mainMenu():
 
 
 
-
+#Page system
 
 
 page="mainMenu"
